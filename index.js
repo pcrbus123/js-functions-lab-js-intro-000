227f561e6eee0897b92c3e91ee971f646275a740
Pnina Breslav <pcrbus123@gmail.com>
2018-10-24 08:03:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // write your code below!
-function happyHolidays(string){console.log("Happy holidays!")}
+function happyHolidays(){
+  console.log("Happy holidays!")
+}
+happyHolidays
 function happyHolidayTo(name) {return `Happy holidays, ${name}!`}
 function happyHolidaysTo(holiday, name){return `Happy ${holiday}, ${name}`}
 function holidayCountdown(days, holiday){return`It's ${days} days until ${holiday}!`}
